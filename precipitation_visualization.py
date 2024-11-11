2f85676eccaca9b8205803575c3e8c5040e112b3
vrjvicente <vrjvicente@outlook.com>
2024-11-11 13:36:22 -0800

    Add exception to user error and visuals.

--- precipitation_visualization.py
+++ precipitation_visualization.py
@@ -11,15 +11,53 @@ instruction = ("\nMake sure the CSV file is stored in the "
 
 print(intro, instruction)
 
-file_name = input("\nPlease enter the CSV file you want to use for the data.\n"
-                  "> ")
-file_name.removesuffix('.csv')
+while True:
+    file_name = input("\nPlease enter the CSV file you want to use for the data.\n"
+                    "> ")
+    file_name = file_name.removesuffix('.csv')
+    path = Path(f'data/{file_name}.csv')
 
-path = Path(f'data/{file_name}.csv')
-lines = path.read_text(encoding='utf-8').splitlines()
+    try:
+        lines = path.read_text(encoding='utf-8').splitlines()
+    except FileNotFoundError:
+        print(f"The file {file_name}.csv is not in the data folder.")
+    else:
+        break
+
+location = input("Please enter the location of this data.\n"
+                 "> ")
+year = input("Please enter the year of this data.\n"
+             "> ")
 
 reader = csv.reader(lines)
 header_row = next(reader)
 
 for index, column_header in enumerate(header_row):
-    print(index, column_header)
+    if column_header == 'DATE':
+        date_index = index
+    if column_header == 'PRCP':
+        prcp_index = index
+
+dates, prcps = [], []
+for row in reader:
+    current_date = datetime.strptime(row[date_index], '%Y-%m-%d')
+    try:
+        prcp = float(row[prcp_index])
+    except:
+        print(f"Missing data for {current_date}")
+    else:
+        dates.append(current_date)
+        prcps.append(prcp)
+
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots(figsize=(12, 6))
+ax.plot(dates, prcps, color='darkturquoise')
+
+ax.set_title(f"Precipitation, {year}"
+             f"\n{location}", fontsize=18)
+ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("Rainfall (mm)", fontsize=16)
+ax.tick_params(labelsize=16)
+
+plt.show()
