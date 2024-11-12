cb6cec220f5027bd2f1dc1720ac9348ac1bb5a04
vrjvicente <vrjvicente@outlook.com>
2024-11-12 12:49:46 -0800

    Add a saving feature.

--- precipitation_visualization.py
+++ precipitation_visualization.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 intro = ("This program shows a visual representation of precipitation "
-               "in a given area from a user's CSV file.")
+         "in a given area from a user's CSV file.")
 instruction = ("\nMake sure the CSV file is stored in the "
                "data folder.")
 
@@ -13,7 +13,7 @@ print(intro, instruction)
 
 while True:
     file_name = input("\nPlease enter the CSV file you want to use for the data.\n"
-                    "> ")
+                      "> ")
     file_name = file_name.removesuffix('.csv')
     path = Path(f'data/{file_name}.csv')
 
@@ -60,4 +60,20 @@ fig.autofmt_xdate()
 ax.set_ylabel("Rainfall (mm)", fontsize=16)
 ax.tick_params(labelsize=16)
 
+while True:
+    save_prompt = ("Would you like to save a copy of the figure? (y/n)\n"
+                   "> ")
+    save_answer = input(save_prompt)
+
+    if save_answer.lower() == 'y':
+        plt.savefig(f'figures/{file_name}.png', bbox_inches='tight')
+        break
+    elif save_answer.lower() == 'n':
+        break
+    else:
+        print("Incorrect answer.")
+        continue
+
 plt.show()
+
+
