cee5b168c069471f672e7a2399c32b45f376a739
vrjvicente <vrjvicente@outlook.com>
2024-11-13 17:33:46 -0800

    Move functions to module

--- precipitation_visualization.py
+++ precipitation_visualization.py
@@ -1,65 +1,7 @@
 from pathlib import Path
 import csv
-from datetime import datetime
 
-import matplotlib.pyplot as plt
-
-def save_figure():
-    """Asks the user if they want a copy of the figure and if so, save
-    the file.
-    """
-    while True:
-        save_prompt = ("Would you like a copy of the figure?\n"
-                       "> ")
-        save_answer = input(save_prompt).lower()
-
-        if save_answer == 'y' or save_answer == 'yes':
-            plt.savefig(f'figures/{file_name}.png', bbox_inches='tight')
-            print(f"\n---------- {file_name}.png has been saved in 'figures' "
-                  "----------\n") 
-            break
-        elif save_answer == 'n' or save_answer == 'no':
-            break
-        else:
-            print("\nInvalid answer. Please try again.")
-
-def generate_figure(dates, prcps, loc, year):
-    """Generates and views the figure using the given parameters."""
-    plt.style.use('seaborn-v0_8')
-    fig, ax = plt.subplots(figsize=(12, 6))
-    ax.plot(dates, prcps, color='darkturquoise')
-
-    ax.set_title(f"Precipitation from {year}"
-                f"\n{loc}", fontsize=18)
-    ax.set_xlabel('', fontsize=16)
-    fig.autofmt_xdate()
-    ax.set_ylabel("Rainfall (mm)", fontsize=16)
-    ax.tick_params(labelsize=16)
-
-    save_figure()
-    print("\n---------- Currently viewing figure... ----------\n")
-    plt.show()
-
-def data_extract(reader):
-    """Gathers the dates and precipitation from the CSV file and asks the
-    user for the location and timeframe of the data.
-    """
-    dates, prcps = [], []
-    for row in reader:
-        current_date = datetime.strptime(row[date_index], '%Y-%m-%d')
-        try:
-            prcp = float(row[prcp_index])
-        except:
-            print(f"Missing data for {current_date}")
-        else:
-            dates.append(current_date)
-            prcps.append(prcp)
-    location = input("Please enter the location of this data.\n"
-                     "> ")
-    year = input("Please enter the timeframe of this data.\n"
-                 "> ")
-    print()
-    generate_figure(dates, prcps, location, year)
+import generate_figure
 
 def collect_index(header, string):
     """Scans for the desired category and return its index."""
@@ -98,9 +40,10 @@ else:
 
     print("\n---------- Collecting data... ----------\n")
     if not date_index or not prcp_index:
-        print("\nWas unable to find the precipitation data "
+        print("Was unable to find the precipitation data "
               "for that file.\n"
               "Please check the file for the precipitation (PRCP) "
               "and its date (DATE).")
     else:
-        data_extract(reader)
+        generate_figure.data_extract(reader, date_index, prcp_index,
+                                     file_name)
